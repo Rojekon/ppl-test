c2d517838fcf87d30abf390327fa1e8666d93a71
Rody <rodionbabkin00@gmail.com>
2022-10-16 20:49:24 +0500

    option and theme styles

--- src/Components/ThemesList.js
+++ src/Components/ThemesList.js
@@ -26,78 +26,116 @@ function ThemesList({ onChange }) {
     <div className="theme-list">
       <h1>Choose your themes:</h1>
       <div>
-        <input
-          type="checkbox"
-          id="1"
-          name="themes"
-          value="1"
-          onChange={handleChange}
-        />{" "}
-        1. Právní předpisy v oblasti letectví
-        <input
-          type="checkbox"
-          id="2"
-          name="themes"
-          value="2"
-          onChange={handleChange}
-        />{" "}
-        2. Lidská výkonnost
-        <input
-          type="checkbox"
-          id="3"
-          name="themes"
-          value="3"
-          onChange={handleChange}
-        />{" "}
-        3. Meteorologie
-        <input
-          type="checkbox"
-          id="4"
-          name="themes"
-          value="4"
-          onChange={handleChange}
-        />{" "}
-        4. Komunikace
-        <input
-          type="checkbox"
-          id="5"
-          name="themes"
-          value="5"
-          onChange={handleChange}
-        />{" "}
-        5. Letové zásady
-        <input
-          type="checkbox"
-          id="6"
-          name="themes"
-          value="6"
-          onChange={handleChange}
-        />{" "}
-        6. Provozní postupy
-        <input
-          type="checkbox"
-          id="7"
-          name="themes"
-          value="7"
-          onChange={handleChange}
-        />{" "}
-        7. Provedení a plánování letu
-        <input
-          type="checkbox"
-          id="8"
-          name="themes"
-          value="8"
-          onChange={handleChange}
-        />{" "}
-        8. Obecné znalosti o letadle
-        <input
-          type="checkbox"
-          id="9"
-          name="themes"
-          value="9"
-          onChange={handleChange}
-        />{" "}
-        9. Navigace
+        <ul className="checkboxes">
+          <li className="checkbox-group">
+            <input
+              type="checkbox"
+              id="1"
+              name="themes"
+              value="1"
+              onChange={handleChange}
+            />{" "}
+            <label for="1" className="label">
+              1. Právní předpisy v oblasti letectví
+            </label>
+          </li>
+          <li className="checkbox-group">
+            <input
+              type="checkbox"
+              id="2"
+              name="themes"
+              value="2"
+              onChange={handleChange}
+            />{" "}
+            <label for="2" className="label">
+              2. Lidská výkonnost
+            </label>
+          </li>
+          <li className="checkbox-group">
+            <input
+              type="checkbox"
+              id="3"
+              name="themes"
+              value="3"
+              onChange={handleChange}
+            />{" "}
+            <label for="3" className="label">
+              3. Meteorologie
+            </label>
+          </li>
+          <li className="checkbox-group">
+            <input
+              type="checkbox"
+              id="4"
+              name="themes"
+              value="4"
+              onChange={handleChange}
+            />{" "}
+            <label for="4" className="label">
+              4. Komunikace
+            </label>
+          </li>
+          <li className="checkbox-group">
+            <input
+              type="checkbox"
+              id="5"
+              name="themes"
+              value="5"
+              onChange={handleChange}
+            />{" "}
+            <label for="5" className="label">
+              5. Letové zásady
+            </label>
+          </li>
+          <li className="checkbox-group">
+            <input
+              type="checkbox"
+              id="6"
+              name="themes"
+              value="6"
+              onChange={handleChange}
+            />{" "}
+            <label for="6" className="label">
+              6. Provozní postupy
+            </label>
+          </li>
+          <li className="checkbox-group">
+            <input
+              type="checkbox"
+              id="7"
+              name="themes"
+              value="7"
+              onChange={handleChange}
+            />{" "}
+            <label for="7" className="label">
+              7. Provedení a plánování letu
+            </label>
+          </li>
+          <li className="checkbox-group">
+            <input
+              type="checkbox"
+              id="8"
+              name="themes"
+              value="8"
+              onChange={handleChange}
+            />{" "}
+            <label for="8" className="label">
+              8. Obecné znalosti o letadle
+            </label>
+          </li>
+          <li className="checkbox-group">
+            <input
+              type="checkbox"
+              id="9"
+              name="themes"
+              value="9"
+              onChange={handleChange}
+            />{" "}
+            <label for="9" className="label">
+              9. Navigace
+            </label>
+          </li>
+        </ul>
       </div>
     </div>
   );
